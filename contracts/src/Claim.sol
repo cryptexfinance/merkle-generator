08adf8af014ea378404bafaa3075919c70d29571
Crisgarner <@crisgarner>
2023-12-12 16:14:19 -0600

    added tests

--- contracts/src/Claim.sol
+++ contracts/src/Claim.sol
@@ -3,25 +3,37 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-error InvalidProof();
-
-contract Claim {
+contract Claim is Ownable {
     bytes32 public root;
     address public immutable treasury;
     ERC20 public immutable rewardToken;
     uint256 public timeout;
-    mapping(uint256 => mapping( address => bool)) public claims;
-    uint256 currentEpoch;
+    uint256 public claimPeriod;
+    mapping(uint256 => mapping(address => bool)) public claims;
+    uint256 public currentEpoch;
 
-    constructor(bytes32 _root, address _treasury, ERC20 _rewardToken, uint256 _timeout) {
+    error InvalidProof();
+    error AlreadyClaimed();
+    error ClaimPeriodExpired();
+    error ClaimPeriodNotExpired();
+
+    constructor(
+        bytes32 _root,
+        address _treasury,
+        ERC20 _rewardToken,
+        uint256 _timeout
+    ) Ownable(msg.sender) {
         root = _root;
         treasury = _treasury;
         rewardToken = _rewardToken;
         timeout = _timeout;
+        /// @notice We assume deploy starts the first reward epoch
+        claimPeriod = block.timestamp + _timeout;
     }
 
-    function isClaimed(address _account) public view returns(bool) {
+    function isClaimed(address _account) public view returns (bool) {
         return claims[currentEpoch][_account];
     }
 
@@ -30,21 +42,35 @@ contract Claim {
         address _account,
         uint256 _amount
     ) public {
+        if(block.timestamp > claimPeriod){
+            revert ClaimPeriodExpired();
+        }
+        if (isClaimed(_account)) {
+            revert AlreadyClaimed();
+        }
         bytes32 leaf = keccak256(
             bytes.concat(keccak256(abi.encode(_account, _amount)))
         );
-        if(!MerkleProof.verify(proof, root, leaf)){
+        if (!MerkleProof.verify(proof, root, leaf)) {
             revert InvalidProof();
         }
-        rewardToken.transfer(_account,_amount);
+        rewardToken.transfer(_account, _amount);
         claims[currentEpoch][_account] = true;
     }
 
-    function endAirdrop() public {
-      require(
-         block.timestamp > timeout,
-         "Claim: Timeout hasn't expired"
-      );
-      rewardToken.transfer(treasury, rewardToken.balanceOf(address(this)));
-   }
+    function newEpoch(bytes32 _root) public onlyOwner {
+        if(block.timestamp <= claimPeriod){
+            revert ClaimPeriodNotExpired();
+        }
+        root = _root;
+        currentEpoch++;
+        claimPeriod = block.timestamp + timeout;
+    }
+
+        function endAirdrop() public onlyOwner {
+        if(block.timestamp <= claimPeriod){
+            revert ClaimPeriodNotExpired();
+        }
+        rewardToken.transfer(treasury, rewardToken.balanceOf(address(this)));
+    }
 }
--- contracts/test/Claim.t.sol
+++ contracts/test/Claim.t.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {Claim, InvalidProof} from "../src/Claim.sol";
+import "../src/Claim.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ClaimTest is Test {
     // the identifiers of the forks
@@ -23,64 +24,48 @@ contract ClaimTest is Test {
             0x581a2e80c5c62766708abb9f162e2a52b55dd4ba0bafe917881389da1d6a76d6
         )
     ];
-    address account2 = 0xFa6863A6507c94ed52e9276F8A72479924E77a36;
+    bytes32 merkleRoot2 =
+        0xef9783ca85748ad7f54f24f9a88ec14ba2ab3c8341b5831d03d0ae428ecbcceb;
     bytes32[] merkleProof2 = [
         bytes32(
-            0xd5f83ff4c6a8aeac795e6eb5299f189d1b3607c97d2437d8429b33ad8daa95ec
+            0xb20c626570fbfd805f8f925a84ebd487bd84e1536f833ade93ebe7d03a4724e1
         ),
         bytes32(
-            0xcaa3f5d0507f2afd15a0ceeb98651a72787d45010683f4904d2320e37649ca27
-        ),
-        bytes32(
-            0x868f547c5f517c4a7431baf4f2c63a9eee71159a37b7228b7ca503e8420b69bd
-        ),
-        bytes32(
-            0x0dfa4119ebd28170dc436f4b1e0ec0292cf402eb443713cd0e66b8bec9b717bb
-        ),
-        bytes32(
-            0xc818964e0b2b91dcaa9806e807da3cb7fd7ecc97daced9830ea2e8515a6500ff
-        ),
-        bytes32(
-            0x49378176c3d375977f0cfea5ea21c593ae4ccf0dca8144bd77119ead42bb060c
-        ),
-        bytes32(
-            0x047fe3d4474b8e980fc72bca8f3e338b722c49b5124a4d4948d30d2313cc1a8b
-        ),
-        bytes32(
-            0xf3f0cc06e84f018ce89e529119dfe5d3435ff03d8d72cf85ccc94945cda29596
+            0x022fb3149ddd5136ffa457821dbffdd9d4ef9cfaca49f1938b4b3c0011339080
         )
     ];
-    address account3 = 0xEf6FE9C9B351824c96e5C7a478C1e52BAdCBAEe0;
+    address account2 = 0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045;
 
     function setUp() public {
         //set fork
         mainnetFork = vm.createSelectFork(vm.rpcUrl("mainnet"), blockNumber);
-
         claim = new Claim(merkleRoot, treasury, ctx, timeout);
         vm.prank(treasury);
         ctx.transfer(address(claim), 50_000 ether);
     }
 
-    function test_SetUpState() public view{
+    function test_SetUpState() public view {
         assert(claim.root() == merkleRoot);
         assert(address(claim.rewardToken()) == address(ctx));
         assert(claim.timeout() == 4 weeks);
         assert(ctx.balanceOf(address(claim)) == 50_000 ether);
         assert(claim.treasury() == treasury);
+        assert(claim.owner() == address(this));
+        assert(claim.claimPeriod() == block.timestamp + timeout);
     }
 
     function test_claim_Revert_WhenInvalidProof() public {
-        vm.expectRevert(InvalidProof.selector);
+        vm.expectRevert(Claim.InvalidProof.selector);
         claim.claim(merkleProof2, account1, 50 ether);
     }
 
     function test_claim_Revert_WhenInvalidValue() public {
-        vm.expectRevert(InvalidProof.selector);
+        vm.expectRevert(Claim.InvalidProof.selector);
         claim.claim(merkleProof1, account1, 250 ether);
     }
 
     function test_claim_Revert_WhenInvalidAccount() public {
-        vm.expectRevert(InvalidProof.selector);
+        vm.expectRevert(Claim.InvalidProof.selector);
         claim.claim(merkleProof1, account2, 50 ether);
     }
 
@@ -89,39 +74,116 @@ contract ClaimTest is Test {
         assertEq(ctx.balanceOf(account1), 0, "Balance not 0");
         claim.claim(merkleProof1, account1, 100 ether);
         assertEq(ctx.balanceOf(account1), 100 ether, "Balance not 100 CTX");
-        assertEq(ctx.balanceOf(address(claim)), (50_000 ether - 100 ether), "Claim contract balance didn't change");
+        assertEq(
+            ctx.balanceOf(address(claim)),
+            (50_000 ether - 100 ether),
+            "Claim contract balance didn't change"
+        );
         assertEq(claim.isClaimed(account1), true, "isClaimed should be true");
     }
 
-    //reset epochs, only claim after epoch
+    function test_claim_WhenCalledFromOtherAddress() public {
+        assertEq(claim.isClaimed(account1), false, "already claimed");
+        assertEq(ctx.balanceOf(account1), 0, "Balance not 0");
+        vm.prank(account2);
+        claim.claim(merkleProof1, account1, 100 ether);
+        assertEq(ctx.balanceOf(account1), 100 ether, "Balance not 100 CTX");
+        assertEq(
+            ctx.balanceOf(address(claim)),
+            (50_000 ether - 100 ether),
+            "Claim contract balance didn't change"
+        );
+        assertEq(claim.isClaimed(account1), true, "isClaimed should be true");
+    }
 
-    // function test_claimFrom() public {
-    //     ctx.transfer(address(claim), 10_000 ether);
-    //     assert(claim.isClaimed(1) == false);
-    //     assertEq(ctx.balanceOf(account2), 0);
-    //     // user1.doClaim(claim, 1, account2, 50 ether, merkleProof2);
-    //     assertEq(ctx.balanceOf(account2), 50 ether);
-    //     assertEq(ctx.balanceOf(address(claim)), (10_000 ether - 50 ether));
-    //     assertTrue(claim.isClaimed(1));
-    // }
+    function test_claim_ShouldFail_IfClaimed() public {
+        claim.claim(merkleProof1, account1, 100 ether);
+        vm.expectRevert(Claim.AlreadyClaimed.selector);
+        claim.claim(merkleProof1, account1, 100 ether);
+        assertEq(ctx.balanceOf(account1), 100 ether, "Balance not 100 CTX");
+        assertEq(
+            ctx.balanceOf(address(claim)),
+            (50_000 ether - 100 ether),
+            "Claim contract balance didn't change"
+        );
+        assertEq(claim.isClaimed(account1), true, "isClaimed should be true");
+    }
 
-    // function testFail_alreadyClaimed() public {
-    //     ctx.transfer(address(claim), 10_000 ether);
-    //     claim.claim(0, account1, 50 ether, merkleProof1);
-    //     // user1.doClaim(claim, 0, account1, 50 ether, merkleProof1);
-    // }
+    function test_claim_ShouldFail_IfClaimPeriodExpired() public {
+        vm.warp(claim.claimPeriod() + 1 seconds);
+        vm.expectRevert(Claim.ClaimPeriodExpired.selector);
+        claim.claim(merkleProof1, account1, 100 ether);
+    }
 
-    // function testFail_endAirdrop() public {
-    //     ctx.transfer(address(claim), 10_000 ether);
-    //     claim.endAirdrop();
-    // }
+    function test_endAirdrop_ShouldFail_WhenNotOwner() public {
+        vm.prank(account1);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Ownable.OwnableUnauthorizedAccount.selector,
+                account1
+            )
+        );
+        claim.endAirdrop();
+    }
 
-    // function test_endAirdrop() public {
-    //     assertEq(ctx.balanceOf(treasury), 0);
-    //     ctx.transfer(address(claim), 10_000 ether);
-    //     vm.warp(4 weeks + 1 seconds);
-    //     claim.endAirdrop();
-    //     assertEq(ctx.balanceOf(address(claim)), 0);
-    //     assertEq(ctx.balanceOf(treasury), 10_000 ether);
-    // }
+    function test_endAirdrop_ShouldFail_WhenClaimPeriodNotExpired() public {
+        vm.expectRevert(Claim.ClaimPeriodNotExpired.selector);
+        claim.endAirdrop();
+    }
+
+    function test_endAirdrop_ShouldTransferTokens_WhenExpired() public {
+        uint256 oldBalance = ctx.balanceOf(treasury);
+        vm.warp(claim.claimPeriod() + 1 seconds);
+        claim.endAirdrop();
+        assert(ctx.balanceOf(address(claim)) == 0);
+        assert(ctx.balanceOf(treasury) == oldBalance + 50_000 ether);
+    }
+
+    function test_newEpoch_ShouldRevert_WhenClaimPeriodNotExpired() public {
+        vm.expectRevert(Claim.ClaimPeriodNotExpired.selector);
+        claim.newEpoch(merkleRoot2);
+    }
+
+    function test_newEpoch_ShouldRevert_WhenNotOwner() public {
+        vm.prank(account1);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Ownable.OwnableUnauthorizedAccount.selector,
+                account1
+            )
+        );
+        claim.newEpoch(merkleRoot2);
+    }
+
+    function test_newEpoch_ShouldStartNewEpoch() public {
+          assertEq(claim.isClaimed(account1), false, "already claimed");
+        assertEq(ctx.balanceOf(account1), 0, "Balance not 0");
+        claim.claim(merkleProof1, account1, 100 ether);
+        assertEq(ctx.balanceOf(account1), 100 ether, "Balance not 100 CTX");
+        assertEq(
+            ctx.balanceOf(address(claim)),
+            (50_000 ether - 100 ether),
+            "Claim contract balance didn't change"
+        );
+        assertEq(claim.isClaimed(account1), true, "isClaimed should be true");
+
+        uint256 oldEpoch = claim.currentEpoch();
+        uint256 oldPeriod = claim.claimPeriod() + 1 seconds;
+        vm.warp(oldPeriod);
+        claim.newEpoch(merkleRoot2);
+        assertEq(claim.currentEpoch(), oldEpoch + 1, "Epoch didn't increase");
+        assertEq(claim.claimPeriod(), oldPeriod + timeout);
+        assertEq(claim.root(), merkleRoot2, "Merkle didn't change");
+        // test claims after
+        assertEq(claim.isClaimed(account1), false, "already claimed");
+        assertEq(ctx.balanceOf(account1), 100 ether, "Balance not 100 CTX");
+        claim.claim(merkleProof2, account1, 100 ether);
+        assertEq(ctx.balanceOf(account1), 200 ether, "Balance not 200 CTX");
+        assertEq(
+            ctx.balanceOf(address(claim)),
+            (50_000 ether - 200 ether),
+            "Claim contract balance didn't change"
+        );
+        assertEq(claim.isClaimed(account1), true, "isClaimed should be true");
+    }
 }
